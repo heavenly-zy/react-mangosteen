f091c131183740b81a3681518aaa71e8651a26ee
ClariS <1457715339@qq.com>
2023-12-23 21:37:29 +0800

    feat: 新增 4 个页面路由

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { NavLink, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
 import ErrorPage from './components/ErrorPage'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>root</div>,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: '/1',
-    element: <div>1</div>,
+    element: <div>根页面<Outlet /></div>,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'welcome',
+        element: <div>Welcome 页面<Outlet /></div>,
+        children: [
+          { path: '1', element: <div> 1 <NavLink to="/welcome/2">下一页</NavLink> </div> },
+          { path: '2', element: <div> 2 <NavLink to="/welcome/3">下一页</NavLink> </div> },
+          { path: '3', element: <div> 3 <NavLink to="/welcome/4">下一页</NavLink> </div> },
+          { path: '4', element: <div> 4 <NavLink to="/start">开始记账</NavLink> </div> }
+        ]
+      },
+    ],
   },
 ])
 
