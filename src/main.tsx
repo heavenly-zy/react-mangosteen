1fbe06e19f5a0a9cc63cd99af3dac2c291e25767
ClariS <1457715339@qq.com>
2023-12-23 20:20:00 +0800

    chore: 删除无用文件

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import './index.css'
 
 const router = createBrowserRouter([
   {
