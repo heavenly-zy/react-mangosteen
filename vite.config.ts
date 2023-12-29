8aaeceec80dda10400c6d61ff190e428d4937ac4
ClariS <1457715339@qq.com>
2023-12-29 16:04:57 +0800

    chore: 配置路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
@@ -5,4 +6,9 @@ import UnoCSS from 'unocss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [UnoCSS(), react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
