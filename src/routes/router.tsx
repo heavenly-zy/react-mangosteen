a803659f60f78cc74af7c5c3a9484d2906fa1d5e
ClariS <1457715339@qq.com>
2023-12-29 21:07:46 +0800

    feat: 添加 404 页面

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,15 +1,15 @@
-import { createBrowserRouter } from "react-router-dom";
-import { MainLayout } from "../layouts/MainLayout";
-import ErrorPage from "../components/ErrorPage";
-import { welcomeRoutes } from "./welcomeRoutes";
+import { createBrowserRouter } from 'react-router-dom'
+import { MainLayout } from '../layouts/MainLayout'
+import { NotFoundPage } from '../pages/NotFoundPage'
+import { welcomeRoutes } from './welcomeRoutes'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
-    errorElement: <ErrorPage />,
+    errorElement: <NotFoundPage />,
     children: [
-      welcomeRoutes
+      welcomeRoutes,
     ],
   },
 ])
