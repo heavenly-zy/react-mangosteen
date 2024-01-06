e9c731b55302226e484c037d7669a25e2f5569e1
ClariS <1457715339@qq.com>
2024-01-06 21:36:37 +0800

    feat: 使用 vite-plugin-mock 在本地创建 mock 服务器

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,24 @@ import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [UnoCSS(), react()],
+export default defineConfig(({ command }) => ({
+  define: {
+    isDev: command === 'serve'
+  },
+  plugins: [
+    UnoCSS(),
+    react(),
+    viteMockServe({
+      mockPath: 'mock',
+      enable: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-})
+}))
