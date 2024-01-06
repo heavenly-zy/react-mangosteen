71e595b2d525d00385a71f76f6fa4f5e6e4e6885
ClariS <1457715339@qq.com>
2024-01-06 22:50:39 +0800

    chore: 更换后端 api 地址

--- src/lib/ajax.ts
+++ src/lib/ajax.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = isDev ? '/' : 'http://121.196.236.94:8080/api/v1'
+axios.defaults.baseURL = isDev ? '/' : 'https://mangosteen2.hunger-valley.com'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 axios.defaults.timeout = 10000
 
