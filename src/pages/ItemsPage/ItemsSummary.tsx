9418dbf0311bc822c37b21a354e4f28582ebf768
ClariS <1457715339@qq.com>
2024-01-10 17:31:14 +0800

    style: ItemsSummary 组件样式

--- src/pages/ItemsPage/ItemsSummary.tsx
+++ src/pages/ItemsPage/ItemsSummary.tsx
@@ -1,5 +1,20 @@
 export const ItemsSummary: React.FC = () => {
   return (
-    <div>ItemsSummary </div>
+    <ol
+      className="m-16px flex items-center justify-between rounded-8px bg-#252A43 px-16px py-12px text-center children-px-24px"
+    >
+      <li text="#FE7275">
+        <div>收入</div>
+        <div>1000</div>
+      </li>
+      <li text="#53A867">
+        <div>支出</div>
+        <div>1000</div>
+      </li>
+      <li text="#ffffff">
+        <div>净收入</div>
+        <div>1000</div>
+      </li>
+    </ol>
   )
 }
