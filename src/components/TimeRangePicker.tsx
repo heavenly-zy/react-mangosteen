761114f8265d59ced92e6da58057a87488f97dbb
ClariS <1457715339@qq.com>
2024-01-10 15:48:45 +0800

    style: TimeRangePicker 组件样式

--- src/components/TimeRangePicker.tsx
+++ src/components/TimeRangePicker.tsx
@@ -1,5 +1,49 @@
-export const TimeRangePicker: React.FC = () => {
+import styled from 'styled-components'
+
+export type TimeRange = 'thisMonth' | 'lastMonth' | 'thisYear' | 'custom'
+
+interface Props {
+  selected: TimeRange
+  onSelected: (selected: TimeRange) => void
+}
+
+const TimeRangeItem = styled.li<{ selected: boolean }>`
+  position: relative;
+  cursor: pointer;
+
+  ${props => props.selected && `
+    &::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 3px;
+      background: #ffffff;
+      opacity: 0.5;
+    }
+  `}
+`
+
+const timeRanges: { key: TimeRange, text: string }[] = [
+  { key: 'thisMonth', text: '本月' },
+  { key: 'lastMonth', text: '上月' },
+  { key: 'thisYear', text: '今年' },
+  { key: 'custom', text: '自定义时间' },
+]
+
+export const TimeRangePicker: React.FC<Props> = ({ selected, onSelected }) => {
   return (
-    <div>TimeRangePicker </div>
+    <ol className="flex text-#ffffff children-px-24px children-py-12px">
+      {timeRanges.map(tr => (
+        <TimeRangeItem
+          key={tr.key}
+          selected={tr.key === selected}
+          onClick={() => onSelected(tr.key)}
+        >
+          {tr.text}
+        </TimeRangeItem>
+      ))}
+    </ol>
   )
 }
--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -1,15 +1,17 @@
-import { TimeRangePicker } from '../components/TimeRangePicker'
+import { useState } from 'react'
+import { type TimeRange, TimeRangePicker } from '../components/TimeRangePicker'
 import { Topnav } from '../components/Topnav'
 import { FloatButton } from '../components/FloatButton'
 import { ItemsList } from './ItemsPage/ItemsList'
 import { ItemsSummary } from './ItemsPage/ItemsSummary'
 
 export const ItemsPage: React.FC = () => {
+  const [timeRange, setTimeRange] = useState<TimeRange>('thisMonth')
   return (
     <div>
-      <div bg-gradient-to-b from='#5c33be' to='#8f4cd7'>
+      <div bg-gradient-to-b from="#5c33be" to="#8f4cd7">
         <Topnav />
-        <TimeRangePicker />
+        <TimeRangePicker selected={timeRange} onSelected={setTimeRange} />
       </div>
       <ItemsSummary />
       <ItemsList />
