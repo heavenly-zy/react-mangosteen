b639c80e7692b9430199efeb16865f62e9a1d133
ClariS <1457715339@qq.com>
2024-01-10 23:44:12 +0800

    style: ItemsList 组件样式

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -7,6 +7,28 @@ import { ItemsSummary } from './ItemsPage/ItemsSummary'
 
 export const ItemsPage: React.FC = () => {
   const [timeRange, setTimeRange] = useState<TimeRange>('thisMonth')
+  const [items] = useState<Item[]>([
+    {
+      id: 1,
+      kind: 'incomes',
+      amount: 1000,
+      user_id: 1,
+      tag_ids: [1],
+      happen_at: '2021-01-01T00:00:00.000Z',
+      created_at: '2021-01-01T00:00:00.000Z',
+      updated_at: '2021-01-01T00:00:00.000Z',
+    },
+    {
+      id: 2,
+      kind: 'incomes',
+      amount: 1000,
+      user_id: 1,
+      tag_ids: [1],
+      happen_at: '2021-01-01T00:00:00.000Z',
+      created_at: '2021-01-01T00:00:00.000Z',
+      updated_at: '2021-01-01T00:00:00.000Z',
+    },
+  ])
   return (
     <div>
       <div bg-gradient-to-b from="#5c33be" to="#8f4cd7">
@@ -14,7 +36,7 @@ export const ItemsPage: React.FC = () => {
         <TimeRangePicker selected={timeRange} onSelected={setTimeRange} />
       </div>
       <ItemsSummary />
-      <ItemsList />
+      <ItemsList items={items} />
       <FloatButton />
     </div>
   )
--- src/pages/ItemsPage/ItemsList.tsx
+++ src/pages/ItemsPage/ItemsList.tsx
@@ -1,5 +1,35 @@
-export const ItemsList: React.FC = () => {
+interface Props {
+  items: Item[]
+}
+export const ItemsList: React.FC<Props> = ({ items }) => {
   return (
-    <div>ItemsList </div>
+    <div>
+      <ol>
+        {items.map(item => (
+          <li key={item.id} grid-cols="[auto_1fr_auto]" className="grid grid-rows-2 gap-x-12px border-b-1 border-b-#eeeeee border-b-solid px-16px py-8px">
+            <div
+              bg="#D8D8D8"
+              rounded="50%"
+              className="col-start-1 row-start-1 col-end-2 row-end-3 h-48px w-48px flex items-center justify-center text-24px"
+            >
+              😘
+            </div>
+            <div className="col-start-2 row-start-1 col-end-3 row-end-2">
+              旅行
+            </div>
+            <div className="col-start-2 row-start-2 col-end-4 row-end-3" text="#999999">
+              2011年1月1日
+            </div>
+            <div className="col-start-3 row-start-1 col-end-4 row-end-2" text="#53A867">
+              ￥999
+            </div>
+          </li>
+        ),
+        )}
+      </ol>
+      <div className="p-16px">
+        <button>加载更多</button>
+      </div>
+    </div>
   )
 }
