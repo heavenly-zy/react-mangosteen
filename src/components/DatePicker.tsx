0a135a6cc0f50a35820cd40ee2582de180be232b
ClariS <1457715339@qq.com>
2024-01-27 12:37:32 +0800

    wip: DatePicker 变为三列

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,28 +1,30 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { time } from '../lib/time'
 
-interface Props {
+interface ColumnProps {
+  className?: string
   start?: Date
   end?: Date
   value?: Date
   itemHeight?: number
 }
 
-export const DatePicker: React.FC<Props> = ({ start, end, value, itemHeight = 36 }) => {
+export const DatePickerColumn: React.FC<ColumnProps> = ({ start, end, value, itemHeight = 36, className }) => {
   const startTime = start ? time(start) : time().add(-10, 'years')
   const endTime = end ? time(end) : time().add(10, 'year')
   const valueTime = value ? time(value) : time()
   if (endTime.timestamp <= startTime.timestamp) {
     throw new Error('结束时间必须晚于开始时间')
   }
-  const years = Array.from({ length: endTime.year - startTime.year + 1 }).map((_, index) => startTime.year + index)
-  const index = years.indexOf(valueTime.year)
+  const yearList = Array.from({ length: endTime.year - startTime.year + 1 }).map((_, index) => startTime.year + index)
+  const index = yearList.indexOf(valueTime.year)
   const [isTouching, setIsTouching] = useState(false)
   const [lastY, setLastY] = useState(0)
   const [translateY, setTranslateY] = useState(index * -itemHeight)
   return (
     <div
-      h="50vh"
+      className={className}
+      h-50vh
       relative
       overflow-hidden
       onTouchStart={(e) => {
@@ -48,17 +50,27 @@ export const DatePicker: React.FC<Props> = ({ start, end, value, itemHeight = 36
           y = translateY - (remainder < -halfItemHeight ? itemHeight + remainder : remainder)
         }
         y = Math.min(y, 0) // y <= 0
-        y = Math.max(y, (years.length - 1) * -itemHeight) // y >= (years.length - 1) * -itemHeight
+        y = Math.max(y, (yearList.length - 1) * -itemHeight) // y >= (years.length - 1) * -itemHeight
         setTranslateY(y)
         setIsTouching(false)
       }}
     >
-      <div style={{ height: itemHeight }} b-1 b-purple b-solid absolute top="1\/2" translate-y="-1\/2" w-full />
-      <div absolute top="1\/2" style={{ transform: `translateY(${-itemHeight / 2}px)` }} b-1 b-red b-solid w-full text-center>
+      <div style={{ height: itemHeight }} b-b-1 b-t-1 b-l-none b-r-none b="#ebedf0" b-solid absolute top="1\/2" translate-y="-1\/2" w-full />
+      <div absolute top="1\/2" style={{ transform: `translateY(${-itemHeight / 2}px)` }} w-full text-center>
         <ol style={{ transform: `translateY(${translateY}px)` }}>
-          {years.map(year => <li key={year} style={{ height: itemHeight, lineHeight: `${itemHeight}px` }}>{year}</li>)}
+          {yearList.map(year => <li key={year} style={{ height: itemHeight, lineHeight: `${itemHeight}px` }}>{year}</li>)}
         </ol>
       </div>
+    </div>
+  )
+}
+
+export const DatePicker: React.FC = () => {
+  return (
+    <div flex>
+      <DatePickerColumn className='grow-1' />
+      <DatePickerColumn className='grow-1' />
+      <DatePickerColumn className='grow-1' />
     </div>
   )
 }
