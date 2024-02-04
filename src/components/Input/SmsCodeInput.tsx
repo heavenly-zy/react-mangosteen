fbec4a76cb6e5eb7604683b50e0ad134e3fa1588
ClariS <1457715339@qq.com>
2024-02-04 22:29:39 +0800

    feat: 完成发送验证码后倒计时的功能

--- src/components/Input/SmsCodeInput.tsx
+++ src/components/Input/SmsCodeInput.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from 'react'
+
 interface Props {
   value?: string
   placeholder?: string
@@ -5,11 +7,35 @@ interface Props {
   request?: () => Promise<unknown>
 }
 
+const countdown = 60
+
 export const SmsCodeInput: React.FC<Props> = ({ value, placeholder, onChange, request }) => {
+  const [startedTime, setStartedTime] = useState<Date>()
+  const [count, setCount] = useState(countdown)
+  const timer = useRef<number>()
   const onClick = async () => {
     if (!request) { return }
-    await request()
+    const result = await request()
+    result && setStartedTime(new Date())
   }
+  const clearTimer = () => {
+    if (!timer.current) { return }
+    window.clearInterval(timer.current)
+    timer.current = undefined
+  }
+  useEffect(() => {
+    if (!startedTime) {
+      clearTimer()
+      return
+    }
+    timer.current = window.setInterval(() => {
+      const seconds = Math.round((new Date().getTime() - startedTime.getTime()) / 1000)
+      const newCount = countdown - seconds
+      if (newCount < 0) { setStartedTime(undefined) }
+      setCount(newCount)
+    }, 1000)
+    return clearTimer
+  }, [startedTime])
   return (
     <div flex gap-x-16px>
       <input
@@ -20,7 +46,17 @@ export const SmsCodeInput: React.FC<Props> = ({ value, placeholder, onChange, re
         value={value}
         onChange={e => onChange?.(e.target.value)}
       />
-      <button type="button" x-btn w="[calc(60%-8px)]" onClick={onClick}>发送验证码</button>
+      {startedTime
+        ? (
+          <button type="button" max-w="[calc(60%-8px)]" shrink-0 x-btn disabled>
+            {`${count} 秒后可重试`}
+          </button>
+          )
+        : (
+          <button type="button" max-w="[calc(60%-8px)]" shrink-0 x-btn onClick={onClick}>
+            发送验证码
+          </button>
+          )}
     </div>
   )
 }
